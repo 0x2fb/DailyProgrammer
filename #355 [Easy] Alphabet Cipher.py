6610cd0474187171f27584c956d707f4f898f58e
0x2fb <36317049+0x2fb@users.noreply.github.com>
2018-04-01 09:23:26 +0200

    Solved Bonus Challenge [Easy 355]

--- #355 [Easy] Alphabet Cipher.py
+++ #355 [Easy] Alphabet Cipher.py
@@ -2,14 +2,29 @@ from string import ascii_lowercase as abc
 import itertools
 
 
-def resultchar(i, j):
-    s = abc.index(j) + abc.index(i)
-    return abc[s - 26] if s > 25 else abc[s]
+def encrypt(codeword, message):
+    def resultchar(i, j):
+        s = abc.index(j) + abc.index(i)
+        return abc[s - 26] if s > 25 else abc[s]
+    codeword = itertools.cycle(codeword)
+    codephrase = [resultchar(i, next(codeword)) for i in message]
+    return "".join(codephrase)
 
 
-codeword, message = input(
-    'Please enter the codeword and your message: ').split()
-codeword = itertools.cycle(codeword)
-codephrase = [resultchar(i, next(codeword)) for i in message]
+def decrypt(codeword, message):
+    def resultchar(i, j):
+        s = abc.index(i) - abc.index(j)
+        return abc[s + 26] if s < 0 else abc[s]
+    codeword = itertools.cycle(codeword)
+    codephrase = [resultchar(i, next(codeword)) for i in message]
+    return "".join(codephrase)
 
-print(f'Your codephrase is: {"".join(codephrase)}')
+
+print(encrypt('snitch', 'thepackagehasbeendelivered'))
+print(encrypt('bond', 'theredfoxtrotsquietlyatmidnight'))
+print(encrypt('train', 'murderontheorientexpress'))
+print(encrypt('garden', 'themolessnuckintothegardenlastnight'))
+
+print(decrypt('cloak', 'klatrgafedvtssdwywcyty'))
+print(decrypt('python', 'pjphmfamhrcaifxifvvfmzwqtmyswst'))
+print(decrypt('moore', 'rcfpsgfspiecbcc'))
